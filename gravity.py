4c93b4de58ab64e98a6e693e082c3dd2d93b10b9
alphonse55 <alphonse.raynaud@icloud.com>
2020-10-08 19:01:25 +0200

    cleaning up

--- gravity.py
+++ gravity.py
@@ -1,12 +1,11 @@
-# IMPORTS
 from matplotlib import pyplot as plt
 import math
 
 # VARIABLES
-planet = str(input("Planet (Earth , Moon , Mars , Jupyter): "))
+planet = input("Planet (Earth , Moon , Mars , Jupyter): ")
 h0 = float(input("Initial height (in m): "))
 v0 = float(input("Initial speed (in m/s): "))
-time = int(input("Time to simulate (in s): "))
+time = float(input("Time to simulate (in s): "))
 dt = float(input("dt: "))
 
 if planet == "Earth":
@@ -21,33 +20,31 @@ elif planet == "Jupyter":
 t = 0
 h = h0
 v = v0
-rebound = False
 s = 0
+
+rebound = False
 
 t_values = []
 h_values = []
 v_values = []
 
+count = []
+zero = []
 
 # OPERATIONS
-for i in range(0, int(time/dt)+1):
-
-    s += dt
+for i in range(0, int(time / dt) + 1):
 
     t_values.append(t)
 
     if rebound == False:
-        v = v0 + a*t
-        h = h0 + v0*t + (a*t**2)/2
+        v = v0 + a * t
+        h = h0 + v0 * t + (a * t ** 2) / 2
     elif rebound == True:
-        v = v0 + a*s
-        h = v*s + (a*s**2)/2
+        v = v0 + a * s
+        h = v * s + (a * s ** 2) / 2
 
     v_values.append(v)
     h_values.append(h)
-
-    # print("v", i, " = ", v)
-    # print("h", i, " = ", h)
 
     if h <= 0:
         v0 = -v
@@ -55,27 +52,18 @@ for i in range(0, int(time/dt)+1):
         s = 0
 
     t += dt
+    s += dt
 
-
-count = []
-zero = []
-
-for i in range(0, time+1):
-    count.append(i)
+    count.append(i * dt)
     zero.append(0)
 
 # PLOT
 plt.figure("Free fall on " + planet)
 plt.plot(t_values, h_values)
-plt.plot(count, zero)
-# plt.plot(t_values, v_values)
+plt.plot(count, zero)  # bottom
+plt.plot(t_values, v_values)
 plt.xlabel("t(s)")
 plt.ylabel("h(m)")
 plt.title("Height - Time")
-
-# plt.figure(2)
-# plt.plot(t_values, v_values)
-# plt.xlabel("t(s)")
-# plt.ylabel("v(m/s)")
 
 plt.show()
